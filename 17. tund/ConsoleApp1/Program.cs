9874656ff22854d6e0d9779ccbcbfd368e5d2973
unknown <emeraldtipyt@gmail.com>
2023-03-08 16:46:16 +0200

    17. tund

---
+++ 17. tund/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int esimenearv;
+            int teinearv;
+            string märk;
+
+            Console.Write("Sisesta esimene arv: "); //ei pane reavahetust lõppu, saad vastata samal real
+            esimenearv = int.Parse(Console.ReadLine()); //sama mis pythony int(input())
+            Console.Write("Sisesta tehtemärk: ");
+            märk = Console.ReadLine();
+            Console.Write("Sisesta teine arv: ");
+            teinearv = int.Parse(Console.ReadLine());
+
+            int vastus = 0;
+
+            switch(märk)
+            {
+                case "+":
+                    vastus = esimenearv + teinearv;
+                    break;
+                case "-":
+                    vastus = esimenearv - teinearv;
+                    break;
+                case "*":
+                    vastus = esimenearv * teinearv;
+                    break;
+                case "/":
+                    vastus = esimenearv / teinearv;
+                    break;
+            }
+
+            Console.WriteLine(vastus.ToString());
+            Console.ReadLine(); //et programm ei väljuks
+        }
+    }
+}
